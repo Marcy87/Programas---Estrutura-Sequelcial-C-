9f96ec972079e5fb8020babe3a2f23bbbb8270c7
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-06 20:03:19 -0300

    Add files via upload

---
+++ Program1009/Program1009/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Program1009
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            double totalMes, salarioFixo, total, comissao;
+
+            nome = Console.ReadLine();
+            totalMes = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            salarioFixo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            comissao = (salarioFixo * 15) / 100;
+            total = comissao + totalMes;
+
+            Console.WriteLine("TOTAL = R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
