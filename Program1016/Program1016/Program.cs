f5f3d0e21d6622992f3abab67057ca8d7cb0234a
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-06 20:05:55 -0300

    Add files via upload

---
+++ Program1016/Program1016/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Program1016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int distancia, t;
+
+            distancia = int.Parse(Console.ReadLine());
+
+            t = distancia * 2;
+
+            Console.WriteLine(t + " minutos");
+        }
+    }
+}
