9dca13607290f9e24c1b6360d175ab0ed72af69d
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-06 20:04:39 -0300

    Add files via upload

---
+++ Program1014/Program1014/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Program1014
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X;
+            double Y, consumoMed;
+
+            X = int.Parse(Console.ReadLine());
+            Y = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            consumoMed = X / Y;
+
+            Console.WriteLine(consumoMed.ToString("F3", CultureInfo.InvariantCulture) + " km/l");
+        }
+    }
+}
