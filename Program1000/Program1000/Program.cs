98f05ca28f75f7f4202999e10489ccf266c1ae1f
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-07 16:20:48 -0300

    Hello World!
    O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!". Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
    
    Entrada
    Este problema não possui nenhuma entrada.
    
    Saída
    Você deve imprimir a mensagem "Hello World!" e em seguida o final de linha, conforme o exemplo abaixo.

---
+++ Program1000/Program1000/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Program1000
+{
+    class Program
+    {
+        static void Main(string[] aargs)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
