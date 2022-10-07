9629a77c6ab51530387753785c35e639f8c96b7d
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-07 16:32:09 -0300

    Cálculo Simples
    Neste problema, deve-se ler o código de uma peça 1, o número de peças 1, o valor unitário de cada peça 1, o código de uma peça 2, o número de peças 2 e o valor unitário de cada peça 2. Após, calcule e mostre o valor a ser pago.
    
    Entrada
    O arquivo de entrada contém duas linhas de dados. Em cada linha haverá 3 valores, respectivamente dois inteiros e um valor com 2 casas decimais.
    
    Saída
    A saída deverá ser uma mensagem conforme o exemplo fornecido abaixo, lembrando de deixar um espaço após os dois pontos e um espaço após o "R$". O valor deverá ser apresentado com 2 casas após o ponto.

---
+++ Program1010/Program1010/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Program1010
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cod1, num1, cod2, num2;
+            double valUni1, valUni2, total;
+
+            string[] vet1 = Console.ReadLine().Split(' ');
+            string[] vet2 = Console.ReadLine().Split(' ');
+
+            cod1 = int.Parse(vet1[0]);
+            num1 = int.Parse(vet1[1]);
+            valUni1 = double.Parse(vet1[2], CultureInfo.InvariantCulture);
+
+            cod2 = int.Parse(vet2[0]);
+            num2 = int.Parse(vet2[1]);
+            valUni2 = double.Parse(vet2[2], CultureInfo.InvariantCulture);
+
+            total = (num1 * valUni1) + (num2 * valUni2);
+
+
+            Console.WriteLine("VALOR A PAGAR: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+         }
+    }
+}
