12bcec97598d3dc6a151500a68bed4be50583687
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-07 16:39:25 -0300

    Gasto de Combustível
    Joaozinho quer calcular e mostrar a quantidade de litros de combustível gastos em uma viagem, ao utilizar um automóvel que faz 12 KM/L. Para isso, ele gostaria que você o auxiliasse através de um simples programa. Para efetuar o cálculo, deve-se fornecer o tempo gasto na viagem (em horas) e a velocidade média durante a mesma (em km/h). Assim, pode-se obter distância percorrida e, em seguida, calcular quantos litros seriam necessários. Mostre o valor com 3 casas decimais após o ponto.
    
    Entrada
    O arquivo de entrada contém dois inteiros. O primeiro é o tempo gasto na viagem (em horas) e o segundo é a velocidade média durante a mesma (em km/h).
    
    Saída
    Imprima a quantidade de litros necessária para realizar a viagem, com três dígitos após o ponto decimal

---
+++ Program1017/Program1017/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Program1016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tempGastoHoras, velocidadeMedia;
+            double qtdLitros, distanciaPercorrida;
+
+            tempGastoHoras = int.Parse(Console.ReadLine());
+            velocidadeMedia = int.Parse(Console.ReadLine());
+
+            distanciaPercorrida = velocidadeMedia * tempGastoHoras;
+
+            qtdLitros = distanciaPercorrida / 12;
+
+            Console.WriteLine(qtdLitros.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
