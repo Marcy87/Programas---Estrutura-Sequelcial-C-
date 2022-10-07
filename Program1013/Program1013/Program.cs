4214780ce58079c6ff6b917a9dbfe185ba890901
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-07 16:35:15 -0300

    O Maior
    Faça um programa que leia três valores e apresente o maior dos três valores lidos seguido da mensagem “eh o maior”.
    
    Obs.: a fórmula apenas calcula o maior entre os dois primeiros (a e b). Um segundo passo, portanto é necessário para chegar no resultado esperado.
    
    Entrada
    O arquivo de entrada contém três valores inteiros.
    
    Saída
    Imprima o maior dos três valores seguido por um espaço e a mensagem "eh o maior".

---
+++ Program1013/Program1013/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Program1013
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z, maiorXY, maior;
+
+            string[] vet = Console.ReadLine().Split(' ');
+
+            x = int.Parse(vet[0]);
+            y = int.Parse(vet[1]);
+            z = int.Parse(vet[2]);
+
+            maiorXY = (x + y + Math.Abs(x - y)) / 2;
+
+            maior = (maiorXY + z + Math.Abs(maiorXY - z)) / 2;
+
+            Console.WriteLine(maior + " eh o maior");
+        }
+
+    }
+}
